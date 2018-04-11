ac0e691208c6def051b89e10030c759e42e27c29
Kaitlyn Robinkoff <35278328+krobinkoff@users.noreply.github.com>
2018-04-10 22:48:06 -0400

    Add files via upload

---
+++ bootstrapinteractive/script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript" language="javascript">
+               window.onbeforeunload = function() {
+                   var Ans = confirm("Are you sure you want to leave?");
+                   if(Ans==true)
+                       return true;
+                   else
+                       return false;
+               };
+</script>
