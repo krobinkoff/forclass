721d60a1543c0cde2d06220b00ea82c79e78bde1
Kaitlyn Robinkoff <35278328+krobinkoff@users.noreply.github.com>
2018-04-11 23:29:31 -0400

    Add files via upload

--- bootstrapinteractive/script.js
+++ bootstrapinteractive/script.js
@@ -1,9 +1,20 @@
-<script type="text/javascript" language="javascript">
-               window.onbeforeunload = function() {
-                   var Ans = confirm("Are you sure you want to leave?");
-                   if(Ans==true)
-                       return true;
-                   else
-                       return false;
-               };
+<script>
+function myFunction() {
+	// Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ul");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
 </script>
